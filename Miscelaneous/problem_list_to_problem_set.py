d2089842c9fddc2793fb675fc97b74274039cf08
ddentona <63802540+ddentona@users.noreply.github.com>
2025-09-02 14:47:22 -0700

    Chapter 20 Problem 1, 3, 5

--- Miscelaneous/problem_list_to_problem_set.py
+++ Miscelaneous/problem_list_to_problem_set.py
@@ -3,4 +3,4 @@ def convertToSections(problem_list: str):
         print("\\section{Problem " + p + "}\n\n    \\subsection{Solution}\n\n\\pagebreak")
 
 if __name__ == "__main__":
-    convertToSections('1, 3, 5, 7, 9, 11, 13, 17, 19, 23, 25, 27, 31, 35, 37, 39, 43, 45, 47, 51, 55, 57, 59, 63, 69, 75, 77')
+    convertToSections('1, 3, 5, 7, 9, 15, 23, 25, 27, 29, 37, 39, 43, 45, 49, 53, 57, 67, 71')
