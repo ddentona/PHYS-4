4e915eb3c913a5493fc12a4e04e5c2e6bea16b62
ddentona <63802540+ddentona@users.noreply.github.com>
2026-02-10 19:01:24 -0800

    Did some coding lol

---
+++ Week 53/Code/hw5.py
@@ -0,0 +1,33 @@
+import matplotlib.pyplot as plt
+import math
+import numpy as np
+
+V0 = 5
+E = 10
+A = 10
+
+def V(x):
+    if x > V0:
+        return 5
+    return 0
+
+def PSI(x):
+    isim = (V(x) - E < 0)
+    psi = A * np.exp((1j if isim else 1) * math.sqrt(abs(E - V(x))) * x)
+    return psi
+
+if __name__ == "__main__":
+    print(np.exp(1j * np.pi))
+
+    myV = np.frompyfunc(V, 1, 1)
+    myPSI = np.frompyfunc(PSI, 1, 1)
+
+    x = np.arange(0,10,0.1)
+    U = myV(x)
+    psi = myPSI(x)
+
+    fig, ax = plt.subplots()
+    ax.plot(x, U)
+    ax.plot(x,psi)
+
+    plt.show()
