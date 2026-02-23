c3d05b7d9e7b9946d7a6dfc63409e10d9270da5d
ddentona <63802540+ddentona@users.noreply.github.com>
2026-02-22 19:36:43 -0800

    HW5 Complete

--- Week 53/Code/hw5.py
+++ Week 53/Code/hw5.py
@@ -2,22 +2,36 @@ import matplotlib.pyplot as plt
 import math
 import numpy as np
 
-L = 4.5
+V_0 = 15
+
 E = 10
 A = 5
+n = 0
+λ = (2*math.pi) / (math.sqrt(abs(E - V_0)))
+L = n * λ
 
 def V(x):
-    if x > L:
-        return 5
+    if x > (n+1) * λ:
+        return 0
+    elif x > n * λ:
+        return V_0
     return 0
 
 def PSI(x):
     isim = (V(x) - E < 0)
-    if x > L:
-        psi = A * np.exp((1j if isim else 1) * (math.sqrt(abs(E - V(x))) * x + math.sqrt(abs(E - V(0))) * L))
+    if x > n * λ:
+        psi = A * np.exp((1j if isim else 1) * (math.sqrt(abs(E - V(x))) * (x - n * λ)))
     else: 
         psi = A * np.exp((1j if isim else 1) * (math.sqrt(abs(E - V(x))) * x))
     return psi
+
+def PSI2(x):
+    if x > L/2:
+        return np.exp(x - L/2)
+    elif x < -L/2:
+        return np.exp(-x - L/2)
+    else:
+        return 0
 
 if __name__ == "__main__":
     print(np.exp(1j * np.pi))
@@ -25,7 +39,7 @@ if __name__ == "__main__":
     myV = np.frompyfunc(V, 1, 1)
     myPSI = np.frompyfunc(PSI, 1, 1)
 
-    x = np.arange(0,10,0.1)
+    x = np.arange(-10,10,0.1)
     U = myV(x)
     psi = myPSI(x)
 
